2b6feb1ed84d89083869b9daa29ea2e874dfcfe8
Adrian Mieńkowski <amienkowski@gmail.com>
2026-07-06 20:23:44 +0200

    add oauth hms-mobile client config

---
+++ src/main/resources/db/migration/V20260706_1925__oauth_hms_mobile_client.sql
@@ -0,0 +1,8 @@
+INSERT INTO oauth2_registered_client (id, client_id, client_id_issued_at, client_secret, client_secret_expires_at,
+                                      client_name, client_authentication_methods, authorization_grant_types,
+                                      redirect_uris, post_logout_redirect_uris, scopes, client_settings, token_settings)
+VALUES ('hms-mobile', 'hms-mobile', now(), NULL, NULL,
+        'HMS Mobile Client', 'none', 'authorization_code',
+        'hms://callback', NULL, 'openid,profile,read',
+        '{"@class":"java.util.Collections$UnmodifiableMap","settings.client.require-proof-key":true,"settings.client.require-authorization-consent":false}',
+        '{"@class":"java.util.Collections$UnmodifiableMap","settings.token.reuse-refresh-tokens":true,"settings.token.x509-certificate-bound-access-tokens":false,"settings.token.id-token-signature-algorithm":["org.springframework.security.oauth2.jose.jws.SignatureAlgorithm","RS256"],"settings.token.access-token-time-to-live":["java.time.Duration",3600.000000000],"settings.token.access-token-format":{"@class":"org.springframework.security.oauth2.server.authorization.settings.OAuth2TokenFormat","value":"self-contained"},"settings.token.refresh-token-time-to-live":["java.time.Duration",86400.000000000],"settings.token.authorization-code-time-to-live":["java.time.Duration",300.000000000],"settings.token.device-authorization-code-time-to-live":["java.time.Duration",300.000000000]}');
